f6f333dd3427576304166f192994453e57e95032
amandavinci <amanthevinci@gmail.com>
2020-04-15 18:18:00 +0200

    add baseline config

---
+++ configs/baseline.py
@@ -0,0 +1,13 @@
+config = {
+    "exp_name": "baseline",
+    "epochs": 20,
+    "encoder": "EmbeddingEncoder",
+    "batch_size": 128,
+    "learning_rate": 1e-3,
+    "seed": 42,
+    "debug": False,
+    "device": 'gpu',
+    "num_workers": 4,
+    "valid_freq": 1000,
+    "save_freq": 2000,
+}
