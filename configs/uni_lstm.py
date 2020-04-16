b5178981cbce91c711a283719d494142988c5adc
amandavinci <amanthevinci@gmail.com>
2020-04-16 08:03:26 +0200

    add experiments

---
+++ configs/uni_lstm.py
@@ -0,0 +1,18 @@
+
+config = {
+    "exp_name": "uni_lstm",
+    "epochs": 20,
+    "encoder": "UniLSTM",
+    "batch_size": 128,
+    # "hidden_dim": 100,
+    "hidden_dim": 2048,
+    "num_layers": 1,
+    "learning_rate": 1e-3,
+    "seed": 42,
+    "debug": False,
+    "device": 'cpu',
+    # "device": 'cuda',
+    "num_workers": 4,
+    "valid_freq": 1000,
+    "save_freq": 2000,
+}
